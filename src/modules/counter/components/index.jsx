f5918a678d4c1988533b0e857dc1fd8ed97019ff
KadoBOT <ricardo.ambrogi@gmail.com>
2017-03-14 12:11:02 -0300

    hoc and redux cycles component

--- src/modules/counter/components/index.jsx
+++ src/modules/counter/components/index.jsx
@@ -1,19 +1,37 @@
 /* eslint no-console: ["error", { allow: ["warn", "error"] }] */
-/* eslint immutable/no-mutation: 0 */
 
 import React from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import {
+  compose,
+  onlyUpdateForPropTypes,
+  pure,
+  setPropTypes,
+  withPropsOnChange,
+} from 'recompose';
 
 import * as actions from '../actions';
 
-const Counter = ({ counter, increment, decrement }) => (
+const enhance = compose(
+  pure,
+  onlyUpdateForPropTypes,
+  setPropTypes({
+    counter: React.PropTypes.number,
+    addOne: React.PropTypes.func,
+    counterLog: React.PropTypes.func,
+  }),
+  withPropsOnChange(['counter'], ({ counter }) => ({ counter })),
+);
+
+const Counter = enhance(({ counter, increment, decrement }) => (
   <div>
-    <h1>{counter}</h1>
+    <h1>Counter module</h1>
+    <p>{counter}</p>
     <button onClick={increment}>+</button>
     <button onClick={decrement}>-</button>
   </div>
-);
+));
 
 const mapStateToProps = state => state;
 
@@ -21,11 +39,5 @@ const mapDispatchToProps = dispatch => bindActionCreators({
   increment: actions.addOne$,
   decrement: actions.removeOne$,
 }, dispatch);
-
-Counter.propTypes = {
-  counter: React.PropTypes.number.isRequired,
-  increment: React.PropTypes.func.isRequired,
-  decrement: React.PropTypes.func.isRequired,
-};
 
 export default connect(mapStateToProps, mapDispatchToProps)(Counter);
